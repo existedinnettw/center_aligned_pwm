1cd564da5682dcf7b30eb2f2b94ca3aefee1e2e1
insleker <bkinnightskytw@gmail.com>
2024-08-19 23:11:19 +0800

    feat: support basic pwm with correct duty cycle

--- src/main.c
+++ src/main.c
@@ -13,64 +13,44 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
 
-static const struct pwm_dt_spec red_pwm_led =
-	PWM_DT_SPEC_GET(DT_ALIAS(red_pwm_led));
-static const struct pwm_dt_spec green_pwm_led =
-	PWM_DT_SPEC_GET(DT_ALIAS(green_pwm_led));
-static const struct pwm_dt_spec blue_pwm_led =
-	PWM_DT_SPEC_GET(DT_ALIAS(blue_pwm_led));
+static const struct pwm_dt_spec u_svpwm_out = PWM_DT_SPEC_GET(DT_NODELABEL(u_svpwm_out));
+static const struct pwm_dt_spec v_svpwm_out = PWM_DT_SPEC_GET(DT_NODELABEL(v_svpwm_out));
+static const struct pwm_dt_spec w_svpwm_out = PWM_DT_SPEC_GET(DT_NODELABEL(w_svpwm_out));
 
 #define STEP_SIZE PWM_USEC(2000)
 
 int main(void)
 {
-	uint32_t pulse_red, pulse_green, pulse_blue; /* pulse widths */
+	// uint32_t pulse_red, pulse_green, pulse_blue; /* pulse widths */
 	int ret;
 
 	printk("PWM-based RGB LED control\n");
 
-	if (!pwm_is_ready_dt(&red_pwm_led) ||
-	    !pwm_is_ready_dt(&green_pwm_led) ||
-	    !pwm_is_ready_dt(&blue_pwm_led)) {
+	if (!pwm_is_ready_dt(&u_svpwm_out) || !pwm_is_ready_dt(&v_svpwm_out) || !pwm_is_ready_dt(&w_svpwm_out))
+	{
 		printk("Error: one or more PWM devices not ready\n");
 		return 0;
 	}
 
-	while (1) {
-		for (pulse_red = 0U; pulse_red <= red_pwm_led.period;
-		     pulse_red += STEP_SIZE) {
-			ret = pwm_set_pulse_dt(&red_pwm_led, pulse_red);
-			if (ret != 0) {
-				printk("Error %d: red write failed\n", ret);
-				return 0;
-			}
+	if ((ret = pwm_set_pulse_dt(&u_svpwm_out, 50000)) != 0)
+	{
+		printk("Error: Failed to set PWM pulse width. Error code: %d\n", ret);
+		return 1;
+	}
+	if ((ret = pwm_set_pulse_dt(&v_svpwm_out, 33336)) != 0)
+	{
+		printk("Error: Failed to set PWM pulse width. Error code: %d\n", ret);
+		return 1;
+	}
+	if ((ret = pwm_set_pulse_dt(&w_svpwm_out, 16667)) != 0)
+	{
+		printk("Error: Failed to set PWM pulse width. Error code: %d\n", ret);
+		return 1;
+	}
 
-			for (pulse_green = 0U;
-			     pulse_green <= green_pwm_led.period;
-			     pulse_green += STEP_SIZE) {
-				ret = pwm_set_pulse_dt(&green_pwm_led,
-						       pulse_green);
-				if (ret != 0) {
-					printk("Error %d: green write failed\n",
-					       ret);
-					return 0;
-				}
-
-				for (pulse_blue = 0U;
-				     pulse_blue <= blue_pwm_led.period;
-				     pulse_blue += STEP_SIZE) {
-					ret = pwm_set_pulse_dt(&blue_pwm_led,
-							       pulse_blue);
-					if (ret != 0) {
-						printk("Error %d: "
-						       "blue write failed\n",
-						       ret);
-						return 0;
-					}
-					k_sleep(K_SECONDS(1));
-				}
-			}
-		}
+	while (1)
+	{
+		k_sleep(K_SECONDS(1));
 	}
 	return 0;
 }
